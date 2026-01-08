a2ce51c80dad6d99ded9964ef65dc4bf7220dc65
Temitayo Adebayo <adebayotemitayo86@gmail.com>
2026-01-08 08:04:50 -0500

    Added top left blur circle

--- app/components/bodyMain/hero.tsx
+++ app/components/bodyMain/hero.tsx
@@ -6,11 +6,12 @@ import Body from "./body";
 export default function HeroBody() {
   return (
     <motion.div
-      className="flex flex-col items-center w-full"
+      className="flex flex-col items-center w-full relative"
       initial={{ opacity: 0, y: 0 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.8, duration: 1.2, ease: [0, 0.59, 0.44, 0.99] }}
     >
+      <div className="absolute top-[-229px] left-[-93px] rounded-full w-[471px] h-[442px] bg-white/70 blur-[121px]"></div>
       {/* navbar */}
       <div className="w-full flex justify-center px-4 md:px-0">
         <Navbar />
